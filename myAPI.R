c5bfdc4a14b3f3ac7cd28a4d98ae89d7c2be2814
Pranav Nair <ppnair@ncsu.edu>
2024-07-29 22:09:59 -0400

    added classification model

--- myAPI.R
+++ myAPI.R
@@ -10,7 +10,8 @@
 
 # Load necessary packages
 library(plumber)
-#library(tidyverse)
+library(tidyverse)
+library(rpart)
 
 # Load the best model
 best_model <- readRDS("best_model.rds")
@@ -18,28 +19,24 @@ best_model <- readRDS("best_model.rds")
 # Load data to determine default values
 data <- read.csv("diabetes_012_health_indicators_BRFSS2015.csv")
 
-# Convert relevant columns to factors and create binary diabetes variable
-# data <- data %>%
-#   mutate(Diabetes_binary = factor(ifelse(Diabetes_012 == 2, "Yes", "No")),  # Create binary diabetes variable as a factor
-#          HighBP = factor(HighBP, levels = c(0, 1), labels = c("No", "Yes")),
-#          HighChol = factor(HighChol, levels = c(0, 1), labels = c("No", "Yes")),
-#          CholCheck = factor(CholCheck, levels = c(0, 1), labels = c("No", "Yes")),
-#          Smoker = factor(Smoker, levels = c(0, 1), labels = c("No", "Yes")),
-#          Stroke = factor(Stroke, levels = c(0, 1), labels = c("No", "Yes")),
-#          HeartDiseaseorAttack = factor(HeartDiseaseorAttack, levels = c(0, 1), labels = c("No", "Yes")),
-#          PhysActivity = factor(PhysActivity, levels = c(0, 1), labels = c("No", "Yes")),
-#          Fruits = factor(Fruits, levels = c(0, 1), labels = c("No", "Yes")),
-#          Veggies = factor(Veggies, levels = c(0, 1), labels = c("No", "Yes")),
-#          HvyAlcoholConsump = factor(HvyAlcoholConsump, levels = c(0, 1), labels = c("No", "Yes")),
-#          AnyHealthcare = factor(AnyHealthcare, levels = c(0, 1), labels = c("No", "Yes")),
-#          GenHlth = factor(GenHlth, levels = 1:5, labels = c("Excellent", "Very Good", "Good", "Fair", "Poor")),
-#          NoDocbcCost = factor(NoDocbcCost, levels = c(0, 1), labels = c("No", "Yes")),
-#          DiffWalk = factor(DiffWalk, levels = c(0, 1), labels = c("No", "Yes")),
-#          Sex = factor(Sex, levels = c(0, 1), labels = c("Female", "Male")),
-#          Age = factor(Age, levels = 1:13, labels = c("18-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", "80+")),
-#          Education = factor(Education, levels = 1:6, labels = c("No Schooling", "Elementary", "Some High School", "High School Grad", "Some College", "College Grad")),
-#          Income = factor(Income, levels = 1:8, labels = c("<$10,000", "$10,000-$15,000", "$15,000-$20,000", "$20,000-$25,000", "$25,000-$35,000", "$35,000-$50,000", "$50,000-$75,000", ">$75,000"))
-#   )
+# Preprocess the data
+data$Diabetes_binary <- factor(ifelse(data$Diabetes_012 == 2, "Yes", "No"))
+
+
+#Set the data into training and test.
+set.seed(123)
+train_index <- sample(seq_len(nrow(data)), size = 0.7 * nrow(data))
+train_data <- data[train_index, ]
+test_data <- data[-train_index, ]
+
+
+class_tree <- train(Diabetes_binary ~ ., 
+                    data = Train_data, 
+                    method = "rpart", 
+                    trControl = train_control, 
+                    metric = "logLoss", 
+                    tuneGrid = expand.grid(cp = seq(from = 0, to = 0.1, by = 0.001)))
+
 
 # Define default values
 default_values <- list(
