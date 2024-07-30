8f2275c4ca411fcf583d132512fc3fd8dc2e0406
Pranav Nair <ppnair@ncsu.edu>
2024-07-29 21:45:19 -0400

    made some additional changes

--- myAPI.R
+++ myAPI.R
@@ -8,68 +8,154 @@
 #
 
 
+# Load necessary packages
 library(plumber)
+#library(tidyverse)
 
+# Load the best model
+best_model <- readRDS("best_model.rds")
 
-best_model <- readRDS("/best_model.rds")
+# Load data to determine default values
+data <- read.csv("diabetes_012_health_indicators_BRFSS2015.csv")
 
-diabetes_data <- read.csv("/diabetes_012_health_indicators_BRFSS2015.csv")
+# Convert relevant columns to factors and create binary diabetes variable
+# data <- data %>%
+#   mutate(Diabetes_binary = factor(ifelse(Diabetes_012 == 2, "Yes", "No")),  # Create binary diabetes variable as a factor
+#          HighBP = factor(HighBP, levels = c(0, 1), labels = c("No", "Yes")),
+#          HighChol = factor(HighChol, levels = c(0, 1), labels = c("No", "Yes")),
+#          CholCheck = factor(CholCheck, levels = c(0, 1), labels = c("No", "Yes")),
+#          Smoker = factor(Smoker, levels = c(0, 1), labels = c("No", "Yes")),
+#          Stroke = factor(Stroke, levels = c(0, 1), labels = c("No", "Yes")),
+#          HeartDiseaseorAttack = factor(HeartDiseaseorAttack, levels = c(0, 1), labels = c("No", "Yes")),
+#          PhysActivity = factor(PhysActivity, levels = c(0, 1), labels = c("No", "Yes")),
+#          Fruits = factor(Fruits, levels = c(0, 1), labels = c("No", "Yes")),
+#          Veggies = factor(Veggies, levels = c(0, 1), labels = c("No", "Yes")),
+#          HvyAlcoholConsump = factor(HvyAlcoholConsump, levels = c(0, 1), labels = c("No", "Yes")),
+#          AnyHealthcare = factor(AnyHealthcare, levels = c(0, 1), labels = c("No", "Yes")),
+#          GenHlth = factor(GenHlth, levels = 1:5, labels = c("Excellent", "Very Good", "Good", "Fair", "Poor")),
+#          NoDocbcCost = factor(NoDocbcCost, levels = c(0, 1), labels = c("No", "Yes")),
+#          DiffWalk = factor(DiffWalk, levels = c(0, 1), labels = c("No", "Yes")),
+#          Sex = factor(Sex, levels = c(0, 1), labels = c("Female", "Male")),
+#          Age = factor(Age, levels = 1:13, labels = c("18-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", "80+")),
+#          Education = factor(Education, levels = 1:6, labels = c("No Schooling", "Elementary", "Some High School", "High School Grad", "Some College", "College Grad")),
+#          Income = factor(Income, levels = 1:8, labels = c("<$10,000", "$10,000-$15,000", "$15,000-$20,000", "$20,000-$25,000", "$25,000-$35,000", "$35,000-$50,000", "$50,000-$75,000", ">$75,000"))
+#   )
 
-#define default values
-default_values <- lapply(diabetes_data, function(column) {
-  if(is.numeric(column)) {
-    mean(column, na.rm = TRUE)
-  } else {
-    as.character(sort(table(column), decreasing = TRUE)[1])
-  }
-})
-
+# Define default values
+default_values <- list(
+  BMI = mean(data$BMI, na.rm = TRUE),
+  HighBP = "No",
+  HighChol = "No",
+  Smoker = "No",
+  CholCheck = "No",
+  Stroke = "No",
+  HeartDiseaseorAttack = "No",
+  PhysActivity = "No",
+  Fruits = "Yes",
+  Veggies = "No",
+  HvyAlcoholConsump = "Yes",
+  AnyHealthcare = "No",
+  NoDocbcCost = "Yes",
+  GenHlth = "Good",
+  MentHlth = 4,
+  PhysHlth = 4,
+  DiffWalk = "No",
+  Sex = "Female",
+  Age = "45-49",
+  Education = "High School Grad",
+  Income = "$50,000-$75,000"
+)
 
 #* @apiTitle Diabetes Prediction API
 
-#* Predict diabetes
-#* @param HighBP
-#* @param HighChol
-#* @param CholCheck
-#* @param BMI
-#* @param Smoker
-#* @param Stroke
-#* @param HeartDiseaseorAttack
-#* @param PhysActivity
-#* @param Fruits
-#* @param Veggies
-#* @param HvyAlcoholConsump
-#* @param AnyHealthcare
-#* @param NoDocbcCost
-#* @param GenHlth
+#* Predict diabetes probability
+#* @param BMI Numeric, Body Mass Index
+#* @param HighBP Character, "Yes" or "No" for high blood pressure
+#* @param HighChol Character, "Yes" or "No" for high cholesterol
+#* @param Smoker Character, "Yes" or "No" for smoking status
+#* @param CholCheck Character, "Yes" or "No" for cholesterol status
+#* @param Stroke Character, "Yes" or "No" for Stroke status
+#* @param HeartDiseaseorAttack Character, "Yes" or "No" for
+#* @param PhysActivity Character, "Yes" or "No" for
+#* @param Fruits Character, "Yes" or "No" for
+#* @param Veggies Character, "Yes" or "No" for
+#* @param HvyAlcoholConsump Character, "Yes" or "No" for
+#* @param AnyHealthcare Character, "Yes" or "No" for
+#* @param NoDocbcCost Character, "Yes" or "No" for
+#* @param GenHlth Character
 #* @param MentHlth
 #* @param PhysHlth
 #* @param DiffWalk
-#* @param Sex
-#* @param Age
-#* @param Education
-#* @param Income
+#* @param Sex Character, "Female" or "Male" for
+#* @param Age Character, Age group
+#* @param Education Character, Education level
+#* @param Income Character, Income bracket
+
+
 #* @post /pred
-function(HighBP = default_values$HighBP, HighChol = default_values$HighChol, CholCheck = default_values$CholCheck,
-         BMI = default_values$BMI, Smoker = default_values$Smoker, Stroke = default_values$Stroke, 
-         HeartDiseaseorAttack = default_values$HeartDiseaseorAttack, PhysActivity = default_values$PhysActivity,
-         Fruits = default_values$Fruits, Veggies = default_values$Veggies, HvyAlcoholConsump = default_values$HvyAlcoholConsump,
-         AnyHealthcare = default_values$AnyHealthcare, NoDocbcCost = default_values$NoDocbcCost, GenHlth = default_values$GenHlth,
-         MentHlth = default_values$MentHlth, PhysHlth = default_values$PhysHlth, DiffWalk = default_values$DiffWalk,
-         Sex = default_values$Sex, Age = default_values$Age, Education = default_values$Education, Income = default_values$Income) {
+function(BMI = default_values$BMI, 
+         HighBP = default_values$HighBP, 
+         HighChol = default_values$HighChol, 
+         Smoker = default_values$Smoker, 
+         CholCheck = default_values$CholCheck,
+         Stroke = default_values$Stroke,
+         HeartDiseaseorAttack = default_values$HeartDiseaseorAttack,
+         PhysActivity = default_values$PhysActivity,
+         Fruits = default_values$Fruits,
+         Veggies = default_values$Veggies,
+         HvyAlcoholConsump = default_values$HvyAlcoholConsump,
+         AnyHealthcare = default_values$AnyHealthcare,
+         NoDocbcCost = default_values$NoDocbcCost,
+         GenHlth = default_values$GenHlth,
+         MentHlth = default_values$MentHlth,
+         PhysHlth = default_values$PhysHlth,
+         DiffWalk = default_values$DiffWalk,
+         Sex = default_values$Sex,
+         Age = default_values$Age,
+         Education = default_values$Education,
+         Income = default_values$Income
+         ) {
   
-  new_data <- data.frame(HighBP, HighChol, CholCheck, BMI, Smoker, Stroke, HeartDiseaseorAttack, PhysActivity,
-                         Fruits, Veggies, HvyAlcoholConsump, AnyHealthcare, NoDocbcCost, GenHlth, MentHlth, 
-                         PhysHlth, DiffWalk, Sex, Age, Education, Income)
+  input_data <- data.frame(
+    BMI = as.numeric(BMI),
+    HighBP = factor(HighBP, levels = c("No", "Yes")),
+    HighChol = factor(HighChol, levels = c("No", "Yes")),
+    Smoker = factor(Smoker, levels = c("No", "Yes")),
+    Age = factor(Age, levels = c("18-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", "80+")),
+    CholCheck = factor(CholCheck, levels = c("No", "Yes")),
+    Stroke = factor(Stroke, levels = c("No", "Yes")),
+    HeartDiseaseorAttack = factor(HeartDiseaseorAttack, levels = c("No","Yes")),
+    PhysActivity = factor(PhysActivity, levels = c("No","Yes")),
+    Fruits = factor(Fruits, levels = c("No","Yes")),
+    Veggies = factor(Veggies, levels = c("No","Yes")),
+    HvyAlcoholConsump = factor(HvyAlcoholConsump, levels = c("No","Yes")),
+    AnyHealthcare = factor(AnyHealthcare, levels = c("No","Yes")),
+    NoDocbcCost = factor(NoDocbcCost, levels = c("No","Yes")),
+    MentHlth = as.numeric(MentHlth),
+    PhysHlth = as.numeric(PhysHlth),
+    GenHlth = factor(GenHlth, levels = 1:5, labels = c("Excellent", "Very Good", "Good", "Fair", "Poor")),
+    DiffWalk = factor(DiffWalk, levels = c("No", "Yes")),
+    Sex = factor(Sex, levels = c("Female","Male")),
+    Age = factor(Age, levels = c("18-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64", "65-69", "70-74", "75-79", "80+")),
+    Education = factor(Education, levels = c("No Schooling", "Elementary", "Some High School", "High School Grad", "Some College", "College Grad")),
+    Income = factor(Income, levels = c("<$10,000", "$10,000-$15,000", "$15,000-$20,000", "$20,000-$25,000", "$25,000-$35,000", "$35,000-$50,000", "$50,000-$75,000", ">$75,000"))
+  )
   
-  predict(best_model, new_data, type = "response")
+  prediction <- predict(best_model, input_data, type = "prob")
+  
+  return(prediction)
 }
 
-#* Get info about the API
+#* API info
 #* @get /info
 function() {
   list(
     name = "Pranav Nair",
-    url = "https://pr4n4vn.github.io/FinalProject/EDA.html"
+    url = "https://pr4n4vn.github.io/FinalProject/"
   )
 }
+
+# Example function calls:
+# curl -X POST "http://localhost:8000/pred?BMI=25&HighBP=No&HighChol=No&Smoker=No&Age=45-49"
+# curl -X POST "http://localhost:8000/pred?BMI=30&HighBP=Yes&HighChol=Yes&Smoker=Yes&Age=60-64"
+# curl -X POST "http://localhost:8000/pred?BMI=22&HighBP=No&HighChol=No&Smoker=No&Age=18-24"
